ebab708ac89ce46abecb8119f721f71586eeb3e9
vijay1811 <vijay1811.kumar@gmail.com>
2017-08-29 10:16:49 +0530

    fixed build error

--- func_test.go
+++ func_test.go
@@ -8,11 +8,11 @@ import (
 
 func slowFunc() {
 	fmt.Println("I am a slow func I will sleep")
-	t.Fatalf("And now I will fail")
 	time.Sleep(5 * time.Second)
 	fmt.Println("I am awake")
 }
 
 func Test_slowFunc(t *testing.T) {
 	slowFunc()
+	t.Fatalf("And now I will fail")
 }
